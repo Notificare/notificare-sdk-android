78e314f68b9670aa99b0725b4f8ca5d1ad448442
Helder Pinhal <helder@notifica.re>
2021-03-03 18:17:00 +0000

    feat(push): handle deep links

--- notificare-push-ui/src/main/java/re/notifica/push/ui/NotificarePushUI.kt
+++ notificare-push-ui/src/main/java/re/notifica/push/ui/NotificarePushUI.kt
@@ -2,6 +2,7 @@ package re.notifica.push.ui
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import re.notifica.Notificare
 import re.notifica.NotificareLogger
 import re.notifica.models.NotificareNotification
@@ -15,13 +16,28 @@ import re.notifica.push.ui.fragments.NotificareVideoFragment
 object NotificarePushUI {
 
     fun presentNotification(activity: Activity, notification: NotificareNotification) {
-        val intent = Intent(Notificare.requireContext(), NotificationActivity::class.java)
-            .putExtra(NotificarePush.INTENT_EXTRA_NOTIFICATION, notification)
-//            .putExtra(Notificare.INTENT_EXTRA_ACTION, action)
-//            .setPackage(Notificare.requireContext().packageName)
+        val type = NotificareNotification.NotificationType.from(notification.type) ?: run {
+            NotificareLogger.warning("Trying to present a notification with an unknown type '${notification.type}'.")
+            return
+        }
 
-        activity.startActivity(intent)
-        activity.overridePendingTransition(0, 0)
+        NotificareLogger.debug("Presenting notification '${notification.id}'.")
+
+        when (type) {
+            NotificareNotification.NotificationType.NONE -> {
+                NotificareLogger.debug("Attempting to present a notification of type 'none'. These should be handled by the application instead.")
+            }
+            NotificareNotification.NotificationType.URL_SCHEME -> presentUrlScheme(activity, notification)
+            else -> {
+                val intent = Intent(Notificare.requireContext(), NotificationActivity::class.java)
+                    .putExtra(NotificarePush.INTENT_EXTRA_NOTIFICATION, notification)
+//                  .putExtra(Notificare.INTENT_EXTRA_ACTION, action)
+//                  .setPackage(Notificare.requireContext().packageName)
+
+                activity.startActivity(intent)
+                activity.overridePendingTransition(0, 0)
+            }
+        }
     }
 
     internal fun getFragmentCanonicalClassName(notification: NotificareNotification): String? {
@@ -84,6 +100,25 @@ object NotificarePushUI {
                 }
             }
             NotificareNotification.NotificationType.VIDEO -> NotificareVideoFragment::class.java.canonicalName
+        }
+    }
+
+    private fun presentUrlScheme(activity: Activity, notification: NotificareNotification) {
+        val content = notification.content.firstOrNull { it.type == "re.notifica.content.URL" } ?: return
+
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(content.data as String)).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
+            setPackage(activity.applicationContext.applicationContext.packageName)
+        }
+
+        // Check if the application can handle the intent itself.
+        if (intent.resolveActivity(activity.applicationContext.packageManager) != null) {
+            activity.startActivity(intent)
+        } else {
+            intent.setPackage(null)
+            if (intent.resolveActivity(activity.applicationContext.packageManager) != null) {
+                activity.startActivity(intent)
+            }
         }
     }
 }
