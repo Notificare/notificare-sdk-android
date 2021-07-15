4db47e09b8ef3c1acd6e80cdf089736f0ef02cb9
Helder Pinhal <helder@notifica.re>
2021-07-15 14:20:52 +0100

    feat(scannables): add base module

---
+++ notificare-scannables/src/main/java/re/notifica/scannables/NotificareScannables.kt
@@ -0,0 +1,18 @@
+package re.notifica.scannables
+
+import re.notifica.modules.NotificareModule
+
+object NotificareScannables : NotificareModule() {
+
+    // region Notificare Module
+
+    override fun configure() {}
+
+    override suspend fun launch() {}
+
+    override suspend fun unlaunch() {}
+
+    // endregion
+
+    //
+}
