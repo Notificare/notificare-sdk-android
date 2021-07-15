df05f07dbaf792579207479de30040463a906af8
Helder Pinhal <helder@notifica.re>
2021-07-15 14:09:21 +0100

    feat(assets): add fetch assets

---
+++ notificare-assets/src/main/java/re/notifica/assets/models/NotificareAsset.kt
@@ -0,0 +1,35 @@
+package re.notifica.assets.models
+
+import android.os.Parcelable
+import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.WriteWith
+import re.notifica.internal.parcelize.NotificareExtraParceler
+
+@Parcelize
+@JsonClass(generateAdapter = true)
+data class NotificareAsset(
+    val title: String,
+    val description: String?,
+    val key: String?,
+    val url: String?,
+    val button: Button?,
+    val metaData: MetaData?,
+    val extra: @WriteWith<NotificareExtraParceler> Map<String, Any> = mapOf(),
+) : Parcelable {
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class Button(
+        val label: String?,
+        val action: String?,
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class MetaData(
+        val originalFileName: String,
+        val contentType: String,
+        val contentLength: Int,
+    ) : Parcelable
+}
