884e452d17580a8c83e54f576fe77a54565f423e
Helder Pinhal <helder@notifica.re>
2021-09-06 16:03:09 +0100

    feat(geo): add module's skeleton

---
+++ notificare-geo/src/main/java/re/notifica/geo/NotificareGeo.kt
@@ -0,0 +1,22 @@
+package re.notifica.geo
+
+import re.notifica.modules.NotificareModule
+
+public object NotificareGeo : NotificareModule() {
+
+    // region NotificareModule
+
+    override fun configure() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun launch() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun unlaunch() {
+        TODO("Not yet implemented")
+    }
+
+    // endregion
+}
