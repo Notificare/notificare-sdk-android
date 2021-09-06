2eea895bf207ff1df0953b0d7c714459168692f3
Helder Pinhal <helder@notifica.re>
2021-09-06 14:19:49 +0100

    feat(core): create opt-in annotation for internal APIs

---
+++ notificare/src/main/java/re/notifica/Annotations.kt
@@ -0,0 +1,16 @@
+package re.notifica
+
+/**
+ * Marks declarations that are **internal** in the Notificare API, which means that should not be used outside of
+ * `re.notifica`, because their signatures and semantics will change between future releases without any
+ * warnings and without providing any migration aids.
+ */
+@Retention(value = AnnotationRetention.BINARY)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.TYPEALIAS, AnnotationTarget.PROPERTY)
+@RequiresOptIn(
+    level = RequiresOptIn.Level.ERROR,
+    message = "This is an internal Notificare API that should not be used from outside of the re.notifica " +
+        "library group. No compatibility guarantees are provided. It is recommended to report your use-case of an " +
+        "internal API to the Notificare issue tracker, so a stable API could be provided instead."
+)
+public annotation class InternalNotificareApi
