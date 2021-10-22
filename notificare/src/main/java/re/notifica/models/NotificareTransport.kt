dcf2be2d64f8dbfd80dcd44fccae8e5c2148a1ea
Helder Pinhal <helder@notifica.re>
2021-10-22 15:45:13 +0100

    refactor(core): remove unnecessary moshi adapter

--- notificare/src/main/java/re/notifica/models/NotificareTransport.kt
+++ notificare/src/main/java/re/notifica/models/NotificareTransport.kt
@@ -1,7 +1,14 @@
 package re.notifica.models
 
+import com.squareup.moshi.Json
+
 public enum class NotificareTransport {
+    @Json(name = "Notificare")
     NOTIFICARE,
+
+    @Json(name = "GCM")
     GCM,
+
+    @Json(name = "HMS")
     HMS
 }
