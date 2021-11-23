ff2809b5d8aa06c226ed8450102746fbf97d0a2d
Helder Pinhal <helder@notifica.re>
2021-11-23 18:37:37 +0000

    feat(assets): add json methods to models

--- notificare-assets/src/main/java/re/notifica/assets/models/NotificareAsset.kt
+++ notificare-assets/src/main/java/re/notifica/assets/models/NotificareAsset.kt
@@ -4,6 +4,9 @@ import android.os.Parcelable
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 import kotlinx.parcelize.WriteWith
+import org.json.JSONObject
+import re.notifica.Notificare
+import re.notifica.internal.moshi
 import re.notifica.internal.parcelize.NotificareExtraParceler
 
 @Parcelize
@@ -18,12 +21,41 @@ public data class NotificareAsset(
     val extra: @WriteWith<NotificareExtraParceler> Map<String, Any> = mapOf(),
 ) : Parcelable {
 
+    public fun toJson(): JSONObject {
+        val jsonStr = adapter.toJson(this)
+        return JSONObject(jsonStr)
+    }
+
+    public companion object {
+        private val adapter = Notificare.moshi.adapter(NotificareAsset::class.java)
+
+        public fun fromJson(json: JSONObject): NotificareAsset {
+            val jsonStr = json.toString()
+            return requireNotNull(adapter.fromJson(jsonStr))
+        }
+    }
+
     @Parcelize
     @JsonClass(generateAdapter = true)
     public data class Button(
         val label: String?,
         val action: String?,
-    ) : Parcelable
+    ) : Parcelable {
+
+        public fun toJson(): JSONObject {
+            val jsonStr = adapter.toJson(this)
+            return JSONObject(jsonStr)
+        }
+
+        public companion object {
+            private val adapter = Notificare.moshi.adapter(Button::class.java)
+
+            public fun fromJson(json: JSONObject): Button {
+                val jsonStr = json.toString()
+                return requireNotNull(adapter.fromJson(jsonStr))
+            }
+        }
+    }
 
     @Parcelize
     @JsonClass(generateAdapter = true)
@@ -31,5 +63,20 @@ public data class NotificareAsset(
         val originalFileName: String,
         val contentType: String,
         val contentLength: Int,
-    ) : Parcelable
+    ) : Parcelable {
+
+        public fun toJson(): JSONObject {
+            val jsonStr = adapter.toJson(this)
+            return JSONObject(jsonStr)
+        }
+
+        public companion object {
+            private val adapter = Notificare.moshi.adapter(MetaData::class.java)
+
+            public fun fromJson(json: JSONObject): MetaData {
+                val jsonStr = json.toString()
+                return requireNotNull(adapter.fromJson(jsonStr))
+            }
+        }
+    }
 }
