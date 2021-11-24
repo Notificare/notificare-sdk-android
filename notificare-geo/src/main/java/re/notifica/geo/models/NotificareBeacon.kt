8e7c8aa60cff92fca39aa81b90a5a6408123c84c
Helder Pinhal <helder@notifica.re>
2021-11-24 17:01:33 +0000

    feat(geo): added missing json methods

--- notificare-geo/src/main/java/re/notifica/geo/models/NotificareBeacon.kt
+++ notificare-geo/src/main/java/re/notifica/geo/models/NotificareBeacon.kt
@@ -2,8 +2,12 @@ package re.notifica.geo.models
 
 import android.os.Parcelable
 import com.squareup.moshi.Json
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
+import org.json.JSONObject
+import re.notifica.Notificare
+import re.notifica.internal.moshi
 
 @Parcelize
 @JsonClass(generateAdapter = true)
@@ -35,3 +39,20 @@ public data class NotificareBeacon(
         FAR;
     }
 }
+
+// region JSON: NotificareBeacon
+
+public val NotificareBeacon.Companion.adapter: JsonAdapter<NotificareBeacon>
+    get() = Notificare.moshi.adapter(NotificareBeacon::class.java)
+
+public fun NotificareBeacon.Companion.fromJson(json: JSONObject): NotificareBeacon {
+    val jsonStr = json.toString()
+    return requireNotNull(adapter.fromJson(jsonStr))
+}
+
+public fun NotificareBeacon.toJson(): JSONObject {
+    val jsonStr = NotificareBeacon.adapter.toJson(this)
+    return JSONObject(jsonStr)
+}
+
+// endregion
--- notificare-geo/src/main/java/re/notifica/geo/models/NotificareLocation.kt
+++ notificare-geo/src/main/java/re/notifica/geo/models/NotificareLocation.kt
@@ -3,8 +3,12 @@ package re.notifica.geo.models
 import android.location.Location
 import android.os.Build
 import android.os.Parcelable
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
+import org.json.JSONObject
+import re.notifica.Notificare
+import re.notifica.internal.moshi
 import java.util.*
 
 @Parcelize
@@ -20,8 +24,6 @@ public data class NotificareLocation(
     val verticalAccuracy: Double,
     val timestamp: Date,
 ) : Parcelable {
-
-    // TODO add JSON methods
 
     public companion object {
         public operator fun invoke(location: Location): NotificareLocation {
@@ -42,3 +44,20 @@ public data class NotificareLocation(
         }
     }
 }
+
+// region JSON: NotificareLocation
+
+public val NotificareLocation.Companion.adapter: JsonAdapter<NotificareLocation>
+    get() = Notificare.moshi.adapter(NotificareLocation::class.java)
+
+public fun NotificareLocation.Companion.fromJson(json: JSONObject): NotificareLocation {
+    val jsonStr = json.toString()
+    return requireNotNull(adapter.fromJson(jsonStr))
+}
+
+public fun NotificareLocation.toJson(): JSONObject {
+    val jsonStr = NotificareLocation.adapter.toJson(this)
+    return JSONObject(jsonStr)
+}
+
+// endregion
