4e9c016313508bb0097e78816905afbc99dc9683
Helder Pinhal <helder@notifica.re>
2021-12-27 11:44:46 +0000

    feat(push): run events with the IO dispatcher

--- notificare-push/src/main/java/re/notifica/push/ktx/Events.kt
+++ notificare-push/src/main/java/re/notifica/push/ktx/Events.kt
@@ -1,16 +1,18 @@
 package re.notifica.push.ktx
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import re.notifica.Notificare
 import re.notifica.NotificareCallback
 import re.notifica.NotificareEventsModule
 import re.notifica.internal.ktx.toCallbackFunction
 
 @Suppress("unused")
-public suspend fun NotificareEventsModule.logNotificationReceived(id: String) {
+public suspend fun NotificareEventsModule.logNotificationReceived(id: String): Unit = withContext(Dispatchers.IO) {
     Notificare.eventsInternal().log(
         event = "re.notifica.event.notification.Receive",
         data = null,
-        notificationId = id
+        notificationId = id,
     )
 }
 
