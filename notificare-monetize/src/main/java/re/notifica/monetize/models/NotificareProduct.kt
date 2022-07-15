8559f6bae14c26ddf491e9a953b18225899f4da3
Helder Pinhal <helder@notifica.re>
2022-07-15 10:04:01 +0100

    feat(monetize): add json utilities for products

--- notificare-monetize/src/main/java/re/notifica/monetize/models/NotificareProduct.kt
+++ notificare-monetize/src/main/java/re/notifica/monetize/models/NotificareProduct.kt
@@ -3,6 +3,9 @@ package re.notifica.monetize.models
 import android.os.Parcelable
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
+import org.json.JSONObject
+import re.notifica.Notificare
+import re.notifica.internal.moshi
 
 @Parcelize
 @JsonClass(generateAdapter = true)
@@ -14,10 +17,23 @@ public data class NotificareProduct(
     val storeDetails: StoreDetails?,
 ) : Parcelable {
 
+    public fun toJson(): JSONObject {
+        val jsonStr = adapter.toJson(this)
+        return JSONObject(jsonStr)
+    }
+
     public companion object {
+        private val adapter = Notificare.moshi.adapter(NotificareProduct::class.java)
+
         public const val TYPE_CONSUMABLE: String = "consumable"
         public const val TYPE_ONE_TIME: String = "onetime"
+
+        public fun fromJson(json: JSONObject): NotificareProduct {
+            val jsonStr = json.toString()
+            return requireNotNull(adapter.fromJson(jsonStr))
+        }
     }
+
 
     @Parcelize
     @JsonClass(generateAdapter = true)
@@ -29,12 +45,42 @@ public data class NotificareProduct(
         // val subscriptionOfferDetails: SubscriptionOfferDetails?,
     ) : Parcelable {
 
+        public fun toJson(): JSONObject {
+            val jsonStr = adapter.toJson(this)
+            return JSONObject(jsonStr)
+        }
+
+        public companion object {
+            private val adapter = Notificare.moshi.adapter(StoreDetails::class.java)
+
+            public fun fromJson(json: JSONObject): StoreDetails {
+                val jsonStr = json.toString()
+                return requireNotNull(adapter.fromJson(jsonStr))
+            }
+        }
+
+
         @Parcelize
         @JsonClass(generateAdapter = true)
         public data class OneTimePurchaseOfferDetails(
             val formattedPrice: String,
             val priceAmountMicros: Long,
             val priceCurrencyCode: String,
-        ) : Parcelable
+        ) : Parcelable {
+
+            public fun toJson(): JSONObject {
+                val jsonStr = adapter.toJson(this)
+                return JSONObject(jsonStr)
+            }
+
+            public companion object {
+                private val adapter = Notificare.moshi.adapter(OneTimePurchaseOfferDetails::class.java)
+
+                public fun fromJson(json: JSONObject): OneTimePurchaseOfferDetails {
+                    val jsonStr = json.toString()
+                    return requireNotNull(adapter.fromJson(jsonStr))
+                }
+            }
+        }
     }
 }
