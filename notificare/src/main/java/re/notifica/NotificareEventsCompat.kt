d30aaa5f10f0188d482de7e72a11915ff01feff1
Yevhenii Smirnov <yevhenii.smirnov@outlook.com>
2022-07-22 11:20:10 +0100

    feat(core): add Java backwards compatibility

---
+++ notificare/src/main/java/re/notifica/NotificareEventsCompat.kt
@@ -0,0 +1,37 @@
+package re.notifica
+
+import re.notifica.ktx.events
+import re.notifica.models.NotificareEventData
+
+public object NotificareEventsCompat {
+
+    @JvmStatic
+    public fun logApplicationException(throwable: Throwable, callback: NotificareCallback<Unit>) {
+        Notificare.events().logApplicationException(throwable, callback)
+    }
+
+    @JvmStatic
+    public fun logNotificationOpen(id: String, callback: NotificareCallback<Unit>) {
+        Notificare.events().logNotificationOpen(id, callback)
+    }
+
+    @JvmStatic
+    public fun logCustom(
+        event: String,
+        callback: NotificareCallback<Unit>
+    ) {
+        Notificare.events().logCustom(
+            event = event,
+            callback = callback
+        )
+    }
+
+    @JvmStatic
+    public fun logCustom(
+        event: String,
+        data: NotificareEventData?,
+        callback: NotificareCallback<Unit>
+    ) {
+        Notificare.events().logCustom(event, data, callback)
+    }
+}
