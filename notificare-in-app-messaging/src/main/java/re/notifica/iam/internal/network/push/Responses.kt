f0c0e2c40964a90fbc5cb7bb8bcce11aa248a3b8
Helder Pinhal <helder@notifica.re>
2022-08-05 09:25:11 +0100

    fix(in-app-messaging): handle defaults for nullable values

--- notificare-in-app-messaging/src/main/java/re/notifica/iam/internal/network/push/Responses.kt
+++ notificare-in-app-messaging/src/main/java/re/notifica/iam/internal/network/push/Responses.kt
@@ -2,27 +2,30 @@ package re.notifica.iam.internal.network.push
 
 import com.squareup.moshi.JsonClass
 import re.notifica.iam.models.NotificareInAppMessage
+import re.notifica.internal.moshi.UseDefaultsWhenNull
 
 @JsonClass(generateAdapter = true)
 internal data class InAppMessageResponse(
     val message: Message,
 ) {
 
+    @UseDefaultsWhenNull
     @JsonClass(generateAdapter = true)
     data class Message(
         val _id: String,
         val name: String,
         val type: String,
-        val context: List<String>,
+        val context: List<String> = listOf(),
         val title: String?,
         val message: String?,
         val image: String?,
         val landscapeImage: String?,
-        val delaySeconds: Int,
+        val delaySeconds: Int = 0,
         val primaryAction: Action?,
         val secondaryAction: Action?,
     ) {
 
+        @UseDefaultsWhenNull
         @JsonClass(generateAdapter = true)
         data class Action(
             val label: String?,
