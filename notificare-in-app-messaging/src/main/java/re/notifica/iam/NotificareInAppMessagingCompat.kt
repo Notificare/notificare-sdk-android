25c1f76e7b3d5c395b04b70d04fcc667601dcd98
Helder Pinhal <helder@notifica.re>
2022-08-30 16:44:39 +0100

    feat(in-app-messaging): add Java wrapper

--- notificare-in-app-messaging/src/main/java/re/notifica/iam/NotificareInAppMessagingCompat.kt
+++ notificare-in-app-messaging/src/main/java/re/notifica/iam/NotificareInAppMessagingCompat.kt
@@ -1,5 +1,24 @@
 package re.notifica.iam
 
+import re.notifica.Notificare
+import re.notifica.iam.ktx.inAppMessaging
+
 public object NotificareInAppMessagingCompat {
-    // TODO: add Java-compat methods & attributes
+
+    @JvmStatic
+    public var hasMessagesSuppressed: Boolean
+        get() = Notificare.inAppMessaging().hasMessagesSuppressed
+        set(value) {
+            Notificare.inAppMessaging().hasMessagesSuppressed = value
+        }
+
+    @JvmStatic
+    public fun addLifecycleListener(listener: NotificareInAppMessaging.MessageLifecycleListener) {
+        Notificare.inAppMessaging().addLifecycleListener(listener)
+    }
+
+    @JvmStatic
+    public fun removeLifecycleListener(listener: NotificareInAppMessaging.MessageLifecycleListener) {
+        Notificare.inAppMessaging().removeLifecycleListener(listener)
+    }
 }
