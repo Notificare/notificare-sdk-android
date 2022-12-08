e7f79f9fd053eb72d5a5a00bd8133f4ed04d94fd
Helder Pinhal <helder@notifica.re>
2022-12-08 15:51:08 +0000

    feat(sample): enable strict mode

--- sample/src/main/java/re/notifica/sample/MainApplication.kt
+++ sample/src/main/java/re/notifica/sample/MainApplication.kt
@@ -1,13 +1,36 @@
 package re.notifica.sample
 
 import android.app.Application
+import android.os.StrictMode
 import re.notifica.Notificare
 
 class MainApplication : Application() {
 
     override fun onCreate() {
+        enableStrictMode()
         super.onCreate()
 
         Notificare.launch()
+    }
+
+    private fun enableStrictMode() {
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .penaltyFlashScreen()
+                .build()
+        )
+
+        StrictMode.setVmPolicy(
+            StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectActivityLeaks()
+                .penaltyLog()
+                .penaltyDeath()
+                .build()
+        )
     }
 }
