8dbd63d6b51ae2dcf3d267d25c953a7bcc232103
João Ferreira <joao@notifica.re>
2024-03-25 08:18:07 +0000

    style(assets): reformat code to comply with ktlint

--- notificare-assets/src/main/java/re/notifica/assets/internal/NotificareAssetsImpl.kt
+++ notificare-assets/src/main/java/re/notifica/assets/internal/NotificareAssetsImpl.kt
@@ -3,7 +3,11 @@ package re.notifica.assets.internal
 import androidx.annotation.Keep
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import re.notifica.*
+import re.notifica.Notificare
+import re.notifica.NotificareApplicationUnavailableException
+import re.notifica.NotificareCallback
+import re.notifica.NotificareNotReadyException
+import re.notifica.NotificareServiceUnavailableException
 import re.notifica.assets.NotificareAssets
 import re.notifica.assets.internal.network.push.FetchAssetsResponse
 import re.notifica.assets.models.NotificareAsset
@@ -46,7 +50,9 @@ internal object NotificareAssetsImpl : NotificareModule(), NotificareAssets {
 
         if (application.services[NotificareApplication.ServiceKeys.STORAGE] != true) {
             NotificareLogger.warning("Notificare storage functionality is not enabled.")
-            throw NotificareServiceUnavailableException(service = NotificareApplication.ServiceKeys.STORAGE)
+            throw NotificareServiceUnavailableException(
+                service = NotificareApplication.ServiceKeys.STORAGE
+            )
         }
     }
 }
