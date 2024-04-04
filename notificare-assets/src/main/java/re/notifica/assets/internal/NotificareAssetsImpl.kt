d5585bcadb33d124034049eaa7fc3102452b0f38
João Ferreira <joao@notifica.re>
2024-04-04 08:09:56 +0100

    style(assets): fix ktlint errors

--- notificare-assets/src/main/java/re/notifica/assets/internal/NotificareAssetsImpl.kt
+++ notificare-assets/src/main/java/re/notifica/assets/internal/NotificareAssetsImpl.kt
@@ -50,9 +50,7 @@ internal object NotificareAssetsImpl : NotificareModule(), NotificareAssets {
 
         if (application.services[NotificareApplication.ServiceKeys.STORAGE] != true) {
             NotificareLogger.warning("Notificare storage functionality is not enabled.")
-            throw NotificareServiceUnavailableException(
-                service = NotificareApplication.ServiceKeys.STORAGE
-            )
+            throw NotificareServiceUnavailableException(service = NotificareApplication.ServiceKeys.STORAGE)
         }
     }
 }
