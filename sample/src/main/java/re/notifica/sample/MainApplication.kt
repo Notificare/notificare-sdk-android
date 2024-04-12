86147cd41b81566f8675ee70400b9612e4391032
Helder Pinhal <helder@notifica.re>
2024-04-12 15:16:47 +0100

    feat(sample): evaluate deferred links

--- sample/src/main/java/re/notifica/sample/MainApplication.kt
+++ sample/src/main/java/re/notifica/sample/MainApplication.kt
@@ -3,6 +3,8 @@ package re.notifica.sample
 import android.app.Application
 import android.os.Build
 import android.os.StrictMode
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 import re.notifica.Notificare
 import re.notifica.geo.ktx.geo
 import re.notifica.push.ktx.push
@@ -10,6 +12,8 @@ import re.notifica.sample.live_activities.LiveActivitiesController
 import timber.log.Timber
 
 class MainApplication : Application() {
+    private val mainScope = MainScope()
+
     override fun onCreate() {
         enableStrictMode()
         super.onCreate()
@@ -27,6 +31,17 @@ class MainApplication : Application() {
         }
 
         Notificare.launch()
+
+        mainScope.launch {
+            try {
+                if (Notificare.canEvaluateDeferredLink()) {
+                    val evaluated = Notificare.evaluateDeferredLink()
+                    Timber.i("deferred link evaluation = $evaluated")
+                }
+            } catch (e: Exception) {
+                Timber.e(e, "Failed to evaluate the deferred link.")
+            }
+        }
     }
 
     private fun enableStrictMode() {
